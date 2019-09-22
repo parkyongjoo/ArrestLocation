043780c45c53fcf397ceb281bef92420b3cfddcc
parkyongjoo <parkyongjoo@gmail.com>
2019-09-22 15:37:12 +0900

    .

--- test.jl
+++ test.jl
@@ -3,7 +3,7 @@ using HTTP
 using JSON
 using DataFrames
 
-#tbi = CSV.read("data/TBI_2011_2016hosp.utf8.csv", delim = "\t", header = 1)
+#tbi = CSV.read("../data/TBI_2011_2016hosp.utf8.csv", delim = "\t", header = 1)
 arrest = CSV.read("data/arrest.utf8.cleared.csv", delim = "\t", header = 1)
 
 #r = HTTP.request("GET", "https://dapi.kakao.com/v2/local/search/address.json"; 
@@ -63,4 +63,65 @@ for i in 1:size(juso, 1)
     end
 
 end
+
+center = CSV.read("../data/center.csv", header = 0)
+centerxy = DataFrame(x = String[], y = String[])
+
+for i in 1:size(center, 1)
+    a = center[i,:Column8]
+    r = HTTP.request("GET", 
+        "https://dapi.kakao.com/v2/local/search/address.json"; 
+        headers = 
+        Dict("Authorization" => "KakaoAK 4566d147680f4ca06a30c943776a3aa5"), 
+        query = Dict("query" => a))
+    j = String(r.body)
+    p = JSON.parse(j)
+    try
+        x = p["documents"][1]["x"]
+        y = p["documents"][1]["y"]
+        println(a," ",x," ",y)
+        push!(centerxy, [x y])
+    catch
+        x = "miss"
+        y = "miss"
+        println(a," ",x," ",y)
+        push!(centerxy, [x y])
+    end
+
+end
+
+kncenterxy = hcat(center, centerxy)
+
+CSV.write("kncenterxy.csv", kncenterxy)
+
+
+facility = CSV.read("../data/facility.csv", header = 0)
+facilityxy = DataFrame(x = String[], y = String[])
+
+for i in 1:size(facility, 1)
+    a = facility[i,:Column8]
+    r = HTTP.request("GET", 
+        "https://dapi.kakao.com/v2/local/search/address.json"; 
+        headers = 
+        Dict("Authorization" => "KakaoAK 4566d147680f4ca06a30c943776a3aa5"), 
+        query = Dict("query" => a))
+    j = String(r.body)
+    p = JSON.parse(j)
+    try
+        x = p["documents"][1]["x"]
+        y = p["documents"][1]["y"]
+        println(a," ",x," ",y)
+        push!(facilityxy, [x y])
+    catch
+        x = "miss"
+        y = "miss"
+        println(a," ",x," ",y)
+        push!(facilityxy, [x y])
+    end
+
+end
+
+knfacilityxy = hcat(facility, facilityxy)
+
+CSV.write("knfacilityxy.csv", knfacilityxy)
 
